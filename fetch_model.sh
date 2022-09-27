40a55deab365c3cbc635f97b7359e4fa6d23801b
Yalım Doğan <yalimdre@gmail.com>
2022-09-27 19:42:03 +0300

    Update fetch_model.sh
    Don't echo the password

--- fetch_model.sh
+++ fetch_model.sh
@@ -4,7 +4,7 @@ urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i
 # SMPL-X 2020 (neutral SMPL-X model with the FLAME 2020 expression blendshapes)
 echo -e "\nYou need to register at https://smpl-x.is.tue.mpg.de"
 read -p "Username (SMPL-X):" username
-read -p "Password (SMPL-X):" password
+read -p "Password (SMPL-X):" -s password
 username=$(urle $username)
 password=$(urle $password)
 wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=SMPLX_NEUTRAL_2020.npz&resume=1' -O './data/SMPLX_NEUTRAL_2020.npz' --no-check-certificate --continue
@@ -12,7 +12,7 @@ wget --post-data "username=$username&password=$password" 'https://download.is.tu
 # PIXIE pretrained model and utilities
 echo -e "\nYou need to register at https://pixie.is.tue.mpg.de/"
 read -p "Username (PIXIE):" username
-read -p "Password (PIXIE):" password
+read -p "Password (PIXIE):" -s password
 username=$(urle $username)
 password=$(urle $password)
 wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=pixie&sfile=pixie_model.tar&resume=1' -O './data/pixie_model.tar' --no-check-certificate --continue
